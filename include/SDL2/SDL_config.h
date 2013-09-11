479821ccf560032e5cd4ac28a79520edd7ae0051
Edward Rudd <urkle@outoforder.cc>
2013-09-11 12:10:01 -0400

    fix missing end parenthesis

--- include/SDL2/SDL_config.h
+++ include/SDL2/SDL_config.h
@@ -39,7 +39,7 @@
 #include "SDL_config_android.h"
 #elif defined(__PSP__)
 #include "SDL_config_psp.h"
-#elif defined(__linux__
+#elif defined(__linux__)
 #include "SDL_config_linux.h"
 #else
 /* This is a minimal configuration just to get SDL running on new platforms */
