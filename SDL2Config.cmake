cde45609ef3c1548661c4fc4f4822e718394131c
Edward Rudd <urkle@outoforder.cc>
2014-11-20 10:50:01 -0500

    add Cmake config scripts. (just add this dir to CMAKE_PREFIX_PATH)

---
+++ SDL2Config.cmake
@@ -0,0 +1,27 @@
+if(APPLE)
+    set(suffix "osx")
+elseif(EMSCRIPTEN)
+    set(suffix "emscripten")
+    message(FATAL_ERROR "No precompiled emscripten SDL2 libs")
+elseif(WIN32)
+    set(suffix "win32")
+    message(FATAL_ERROR "No precompiled Windows SDL2 libs")
+elseif(CMAKE_SYSTEM MATCHES "Linux")
+    set(suffix "linux")
+endif()
+
+find_library(SDL2_LIBRARY
+    NAMES SDL2
+    PATHS "${CMAKE_CURRENT_LIST_DIR}/${suffix}/lib" "${CMAKE_CURRENT_LIST_DIR}/${suffix}"
+    NO_DEFAULT_PATH
+)
+find_path(SDL2_INCLUDE_DIR
+    NAMES SDL.h
+    PATHS "${CMAKE_CURRENT_LIST_DIR}/include"
+    PATH_SUFFIXES SDL2
+    NO_DEFAULT_PATH
+)
+set(SDL2_INCLUDE_DIRS ${SDL2_INCLUDE_DIR})
+set(SDL2_LIBRARIES ${SDL2_LIBRARY})
+
+mark_as_advanced(SDL2_INCLUDE_DIR SDL2_LIBRARY)
