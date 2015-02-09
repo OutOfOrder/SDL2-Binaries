b37a67b8ed93cc525358d7c41d301e3ebd0fe3e7
Edward Rudd <urkle@outoforder.cc>
2015-02-09 12:29:36 -0500

    add searching of SDL2main on platforms that require it.

--- SDL2Config.cmake
+++ SDL2Config.cmake
@@ -15,6 +15,12 @@ find_library(SDL2_LIBRARY
     NO_DEFAULT_PATH
     NO_CMAKE_FIND_ROOT_PATH
 )
+find_library(SDL2_main_LIBRARY
+    NAMES SDL2main
+    PATHS "${CMAKE_CURRENT_LIST_DIR}/${suffix}/lib" "${CMAKE_CURRENT_LIST_DIR}/${suffix}"
+    NO_DEFAULT_PATH
+    NO_CMAKE_FIND_ROOT_PATH
+)
 find_path(SDL2_INCLUDE_DIR
     NAMES SDL.h
     PATHS "${CMAKE_CURRENT_LIST_DIR}/include"
@@ -24,5 +30,10 @@ find_path(SDL2_INCLUDE_DIR
 )
 set(SDL2_INCLUDE_DIRS ${SDL2_INCLUDE_DIR})
 set(SDL2_LIBRARIES ${SDL2_LIBRARY})
+if(SDL2_main_LIBRARY)
+    list(APPEND SDL2_LIBRARIES
+        ${SDL2_main_LIBRARY}
+    )
+endif()
 
-mark_as_advanced(SDL2_INCLUDE_DIR SDL2_LIBRARY)
+mark_as_advanced(SDL2_INCLUDE_DIR SDL2_LIBRARY SDL2_main_LIBRARY)
